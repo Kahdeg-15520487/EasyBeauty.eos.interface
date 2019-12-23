9dd72e3a489de5f1c732b809468166ee072660f4
Merlin Nguyễn <jonhenry13241@gmail.com>
2019-12-23 16:13:58 +0700

    update

--- index.js
+++ index.js
@@ -14,162 +14,104 @@ const coin = require('./coin.js');
 const rv = require('./review.js');
 const discount = require('./discount.js');
 
-const app = express()
-const port = 3000
 
-const privateKey = process.env.pk;
-
-const privateKeys = [privateKey];
-
-const signatureProvider = new JsSignatureProvider(privateKeys);
-const rpc = new JsonRpc(process.env.api, { fetch });
-const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
-
-//api.getContract("store.data").then(c=>{console.log(typeof c);console.log(c);});
-//api.getContract("store.data").then(c=>{console.log(typeof c);console.log(c.actions.get('transact'));});
-
-// api.transact({
-//     actions:[{
-//         account:'store.data',
-//         name:'transact',
-//         authorization:[{
-//             actor:'store.data',
-//             permission:'active'
-//         }],
-//         data:{
-//             user:'store.data',
-//             from:'user1',
-//             to:'user2',
-//             hash:'lala'
-//         }
-//     }]
-// }, {
-//     blocksBehind: 3,
-//     expireSeconds: 30,
-// })
-// .then(v=>{
-//     console.log(v);
-//     console.log(v.processed.receipt);
-//     const txid = v.transaction_id;
-
-
-//     rpc.get_table_rows({
-//         json: true,              // Get the response as json
-//         code: 'store.data',     // Contract that we target
-//         scope: 'store.data',         // Account that owns the data
-//         table: 'transactions',        // Table name
-//     }).then(resp=>{    
-//         console.log("=====================");
-//         console.log(resp.rows);
-//     })
-// },r=>{
-//     console.log("failed");
-//     console.log(r);
-// })
-// .catch(e=>console.log(e))
-// ;
-
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
-
-app.get('/transaction/:txid', async (req, res) => {
-    await tx.get(req,res,api,rpc);
-});
-
-app.post('/transaction/validate', async (req, res) => {
-    await tx.validate(req,res,api,rpc);
-});
-
-app.post('/transaction/create', async (req, res) => {
-    await tx.create(req,res,api,rpc);
-});
-
-app.get('/table/:tb', async (req, res) => {
-    await table.query(req, res, rpc);
-})
-
-app.get('/coin/:usid', async (req, res) => {
-    await coin.get(req, res, api, rpc);
-})
-
-app.post('/coin/update', async (req, res) => {
-    await coin.update(req, res, api, rpc);
-})
-
-app.post('/review',async(req,res)=>{
-    await rv.create(req,res,api,rpc);
-})
-
-app.get('/review/:rvid',async (req,res) => {
-    await rv.get(req,res,api,rpc);
-})
-
-app.post('/review/validate',async (req,res) => {
-    await rv.validate(req,res,api,rpc);
-})
-
-app.post('/discount',async (req,res) => {
-    await discount.create(req,res,api,rpc);
-})
-
-app.get('/discount',async (req,res) => {
-    await discount.get(req,res,api,rpc);
-})
-
-app.get('/mock/transaction', async(req,res)=>{
-    const lala =[];
-    for (let i = 0; i < 5; i++) {
-        const obj = {
-            "txid": util.genId(24),
-            "from": util.genId(24),
-            "to": util.genId(24),
-            "value": util.randomNum(1,30)
-        };
-        const txid = await tx.crtx(obj,api);
-        lala.push({
-            "key": txid,
-            "val": obj
-        });
+class EasyBeautyEOS {
+    constructor(key, node) {
+        signatureProvider = new JsSignatureProvider([key]);
+        this.rpc = new JsonRpc(node, fetch);
+        this.api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
     }
-    res.json(lala);
-})
 
-app.get('/mock/review', async(req,res)=>{
-    const lala =[];
-    for (let i = 0; i < 5; i++) {
-        const obj = {
-            "rvid": util.genId(24),
-            "txid": util.genId(24),
-            "rate": util.randomNum(100,500),
-            "cmt": "good",
-            "time": util.randomNum(20,100)
-        };
-        const txid = await rv.crrv(obj,api);
-        lala.push({
-            "key": txid,
-            "val": obj
-        });
+    async createTransaction(txid, from, to, value) {
+        if (await tx.checkDuplicate(txid, this.rpc)) {
+            return { "status": false, "val": "duplicate transaction" };
+        }
+        return await tx.crtx({
+            "txid": txid,
+            "from": from,
+            "to": to,
+            "value": value
+        }, this.api);
     }
-    res.json(lala);
-})
 
-app.get('/mock/discount', async(req,res)=>{
-    const lala =[];
-    for (let i = 0; i < 5; i++) {
+    async validateTransaction(txid, from, to, value) {
+        const oldtx = await table.getTransaction(txid, this.rpc);
+        console.log(txid);
+
+        if (!oldtx) {
+            return { "status": false, "val": "transaction does not exist" };
+        }
+
         const obj = {
-            "txid": util.genId(24),
-            "usid": util.genId(24),
-            "percent" : util.randomNum(1,100)
+            "txid": req.body.transactionId,
+            "from": req.body.transactionInfo.from,
+            "to": req.body.transactionInfo.to,
+            "value": req.body.transactionInfo.value
         };
-        const txid = await discount.crdc(obj,api);
-        lala.push({
-            "key": txid,
-            "val": obj
-        });
-    }
-    res.json(lala);
-})
 
-app.listen(port, () => console.log(`app listening on port ${port}!`));
+        const h = hash.hashTransaction(obj);
+        const result = oldtx.hash === h;
+        return { "status": true, "val": result };
+    }
+
+    async getTransaction(txid) {
+        console.log(txid);
+        const transaction = await table.getTransaction(txid, this.rpc);
+        if (transaction) {
+            delete transaction.key;
+            return {"status": true, "val": JSON.stringify(transaction)};
+        }
+        else {
+            return { "status": false, "val": "transaction does not exist" };
+        }
+    }
+
+    
+
+    async createReview(rvid, txid, rate, comment, time) {
+        if (await tx.checkDuplicate(rvid, this.rpc)) {
+            return { "status": false, "val": "duplicate review" };
+        }
+        return await tx.crtx({
+            "txid": txid,
+            "from": from,
+            "to": to,
+            "value": value
+        }, this.api);
+    }
+
+    async validateTransaction(txid, from, to, value) {
+        const oldtx = await table.getTransaction(txid, this.rpc);
+        console.log(txid);
+
+        if (!oldtx) {
+            return { "status": false, "val": "transaction does not exist" };
+        }
+
+        const obj = {
+            "txid": req.body.transactionId,
+            "from": req.body.transactionInfo.from,
+            "to": req.body.transactionInfo.to,
+            "value": req.body.transactionInfo.value
+        };
+
+        const h = hash.hashTransaction(obj);
+        const result = oldtx.hash === h;
+        return { "status": true, "val": result };
+    }
+
+    async getTransaction(txid) {
+        console.log(txid);
+        const transaction = await table.getTransaction(txid, this.rpc);
+        if (transaction) {
+            delete transaction.key;
+            return {"status": true, "val": JSON.stringify(transaction)};
+        }
+        else {
+            return { "status": false, "val": "transaction does not exist" };
+        }
+    }
+
+}
+
+module.exports = EasyBeautyEOS;
